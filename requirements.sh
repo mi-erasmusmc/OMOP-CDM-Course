71009ebc4a8b7ac3975356b56b7c62dcfeefc58a
Peter Rijnbeek <p.rijnbeek@erasmusmc.nl>
2017-11-25 22:47:50 +0100

    added vocab

--- requirements.sh
+++ requirements.sh
@@ -7,7 +7,7 @@ apt-get update && apt-get install -y unzip
 
 mkdir -p $REPO_PATH/data
 # You can replace this with a link to your archive of csv files
-wget "https://assets.datacamp.com/course/sql/films.zip"
-unzip films.zip -d $REPO_PATH/data
+wget "https://s3.amazonaws.com/assets.datacamp.com/production/course_6000/datasets/vocab.zip"
+unzip vocab.zip -d $REPO_PATH/data
 
-service postgresql start   && sudo -u postgres createdb -O repl films   && cd $REPO_PATH   && sudo -u postgres psql films < data/films/films.sql   && service postgresql stop
+service postgresql start   && sudo -u postgres createdb -O repl vocab   && cd $REPO_PATH   && sudo -u postgres psql vocab < data/vocab/vocab.sql   && service postgresql stop
