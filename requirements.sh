5591ae581635da8387597da30b1bf8cc4b54a17e
Peter Rijnbeek <p.rijnbeek@erasmusmc.nl>
2017-11-25 23:56:08 +0100

    update

--- requirements.sh
+++ requirements.sh
@@ -7,7 +7,7 @@ apt-get update && apt-get install -y unzip
 
 mkdir -p $REPO_PATH/data
 # You can replace this with a link to your archive of csv files
-wget "https://s3.amazonaws.com/assets.datacamp.com/production/course_6000/datasets/vocab.zip"
-unzip vocab.zip -d $REPO_PATH/data
+wget "https://s3.amazonaws.com/assets.datacamp.com/production/course_6000/datasets/vocab1.zip"
+unzip vocab1.zip -d $REPO_PATH/data
 
 service postgresql start   && sudo -u postgres createdb -O repl vocab1   && cd $REPO_PATH   && sudo -u postgres psql vocab1 < data/vocab1/vocab.sql   && service postgresql stop
