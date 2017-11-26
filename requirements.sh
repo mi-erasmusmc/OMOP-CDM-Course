62d56d14e4c23e48023c2012a4cbc3182cbf85a4
Peter Rijnbeek <p.rijnbeek@erasmusmc.nl>
2017-11-26 12:58:44 +0100

    vocabulary2

--- requirements.sh
+++ requirements.sh
@@ -7,7 +7,7 @@ apt-get update && apt-get install -y unzip
 
 mkdir -p $REPO_PATH/data
 # You can replace this with a link to your archive of csv files
-DATASET=vocabulary1
+DATASET=vocabulary2
 wget "https://s3.amazonaws.com/assets.datacamp.com/production/course_6000/datasets/$DATASET.zip"
 unzip $DATASET.zip -d $REPO_PATH/data
 
