b1b2a7a54c3d3c0762eac33b5382c6c0856c854b
Peter Rijnbeek <p.rijnbeek@erasmusmc.nl>
2017-11-26 12:42:26 +0100

    updated database

--- requirements.sh
+++ requirements.sh
@@ -7,8 +7,9 @@ apt-get update && apt-get install -y unzip
 
 mkdir -p $REPO_PATH/data
 # You can replace this with a link to your archive of csv files
-wget "https://s3.amazonaws.com/assets.datacamp.com/production/course_6000/datasets/vocabulary.zip"
-unzip vocabulary.zip -d $REPO_PATH/data
+DATASET=vocabulary1
+wget "https://s3.amazonaws.com/assets.datacamp.com/production/course_6000/datasets/$DATASET.zip"
+unzip $DATASET.zip -d $REPO_PATH/data
 
-service postgresql start   && sudo -u postgres createdb -O repl vocabulary   && cd $REPO_PATH   && sudo -u postgres psql vocabulary < data/vocabulary/vocabulary.sql   && service postgresql stop
+service postgresql start   && sudo -u postgres createdb -O repl $DATASET   && cd $REPO_PATH   && sudo -u postgres psql $DATASET < data/$DATASET/$DATASET.sql   && service postgresql stop
 # test
